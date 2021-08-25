9ef91434f1b90d034885e202a3c7bcdb47b84fe1
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-08-25 16:04:02 -0300

    adiciona accordion

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,20 @@
-console.log('ola')
+const buttonAccordion = document.querySelectorAll('[data-accordion]');
+
+function toggleAccordion(event) {
+    const element = event.target;
+    const questResponse = element.nextElementSibling;
+
+    element.classList.toggle("active");
+    questResponse.classList.toggle("active");
+
+    if (element.getAttribute("class")) {
+        element.children[0].src = "./assets/icon-arrow-down.svg";
+    } else {
+        element.children[0].src = "./assets/icon-arrow-up.svg";
+
+    }
+}
+
+buttonAccordion.forEach((button) => {
+    button.addEventListener("click", toggleAccordion);
+})
